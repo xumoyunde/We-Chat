9c34d5071288bff532f0522fc89ffdd7a086f79b
xumoyunde <xakimjonovxumoyun7@gmail.com>
2023-05-15 13:32:54 +0500

    firebase initialized app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:we_chat/screens/auth/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 late Size mq;
 
 void main() {
+  _initializeFirebase();
   runApp(const MyApp());
 }
 
@@ -31,4 +34,10 @@ class MyApp extends StatelessWidget {
       home: LoginScreen(),
     );
   }
+}
+
+_initializeFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
