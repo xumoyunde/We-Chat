c09a9e996194ed5edfa68e4106782eb52b50e6d9
xumoyunde <xakimjonovxumoyun7@gmail.com>
2023-05-15 12:25:57 +0500

    added appbar and floating action button

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(CupertinoIcons.home),
+        title: Text('We Chat'),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.more_vert)),
+        ],
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton(onPressed: (){}, child: Icon(Icons.add_comment_rounded)),
+      ),
+    );
+  }
+}
